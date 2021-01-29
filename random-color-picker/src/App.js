8bbbd6571929f1d932f62dbffda566f78e45d867
Eduardo <edalvarez2015@gmail.com>
2021-01-29 16:30:14 -0600

    Project Completed!

---
+++ random-color-picker/src/App.js
@@ -0,0 +1,13 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Random from "./Random";
+import Button from "./Button";
+function App() {
+  return (
+    <div className="App">
+      <Random />
+    </div>
+  );
+}
+
+export default App;
